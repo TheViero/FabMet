73eb44b01eb054cb6a40715cb809c1e3c086e11e
TheViero <128616582+TheViero@users.noreply.github.com>
2023-03-27 17:33:43 +0200

    FabMet

---
+++ FabMet/ClassLibrary1/Class1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        public abstract class Shape
+        {
+            public abstract double GetArea();
+        }
+
+        public class Circle : Shape
+        {
+            private double radius;
+
+            public Circle(double radius)
+            {
+                this.radius = radius;
+            }
+
+            public override double GetArea()
+            {
+                return Math.PI * radius * radius;
+            }
+        }
+
+        public class Rectangle : Shape
+        {
+            private double length;
+            private double width;
+
+            public Rectangle(double length, double width)
+            {
+                this.length = length;
+                this.width = width;
+            }
+
+            public override double GetArea()
+            {
+                return length * width;
+            }
+        }
+
+        public class Triangle : Shape
+        {
+            private double baseLength;
+            private double height;
+
+            public Triangle(double baseLength, double height)
+            {
+                this.baseLength = baseLength;
+                this.height = height;
+            }
+
+            public override double GetArea()
+            {
+                return 0.5 * baseLength * height;
+            }
+        }
+
+        public abstract class ShapeFactory
+        {
+            public abstract Shape CreateShape();
+        }
+
+        public class CircleFactory : ShapeFactory
+        {
+            private double radius;
+
+            public CircleFactory(double radius)
+            {
+                this.radius = radius;
+            }
+
+            public override Shape CreateShape()
+            {
+                return new Circle(radius);
+            }
+        }
+
+        public class RectangleFactory : ShapeFactory
+        {
+            private double length;
+            private double width;
+
+            public RectangleFactory(double length, double width)
+            {
+                this.length = length;
+                this.width = width;
+            }
+
+            public override Shape CreateShape()
+            {
+                return new Rectangle(length, width);
+            }
+        }
+
+        public class TriangleFactory : ShapeFactory
+        {
+            private double baseLength;
+            private double height;
+
+            public TriangleFactory(double baseLength, double height)
+            {
+                this.baseLength = baseLength;
+                this.height = height;
+            }
+
+            public override Shape CreateShape()
+            {
+                return new Triangle(baseLength, height);
+            }
+        }
+    }
+}
---
+++ FabMet/FabMet/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using ClassLibrary1;
+using static ClassLibrary1.Class1;
+
+namespace FabMet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Створення фабрик для круга, прямокутника та трикутника
+            ShapeFactory circleFactory = new CircleFactory(5);
+            ShapeFactory rectangleFactory = new RectangleFactory(10, 5);
+            ShapeFactory triangleFactory = new TriangleFactory(6, 4);
+
+            // Створення фігур з використанням фабрик
+            Shape circle = circleFactory.CreateShape();
+            Shape rectangle = rectangleFactory.CreateShape();
+            Shape triangle = triangleFactory.CreateShape();
+
+            // Виведення площ фігур
+            Console.WriteLine($"Площа круга з радіусом 5: {circle.GetArea()}");
+            Console.WriteLine($"Площа прямокутника з довжиною 10 та шириною 5: {rectangle.GetArea()}");
+            Console.WriteLine($"Площа трикутника з основою 6 та висотою 4: {triangle.GetArea()}");
+        }
+    }
+
+}
